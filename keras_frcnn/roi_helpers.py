5e7a734d1f8968dc32af4e2fcb30b9c40e093267
Gilad Divon <giladd44@gmail.com>
2018-01-16 18:28:18 +0200

    added overlap_ratio function for bbox overlap used in video train

--- keras_frcnn/roi_helpers.py
+++ keras_frcnn/roi_helpers.py
@@ -821,3 +821,63 @@ def overlap_messa(bbox,gt_bbox,ratio=1):
 
 	# return only the bounding boxes that were picked using the integer data type
 	return good_idx
+
+def overlap_ratio(boxes, probs, overlap_thresh=0.7):
+	# code used from here: http://www.pyimagesearch.com/2015/02/16/faster-non-maximum-suppression-python/
+	# if there are no boxes, return an empty list
+	if len(boxes) == 0:
+		return []
+
+	# grab the coordinates of the bounding boxes
+	x1 = boxes[:, 0]
+	y1 = boxes[:, 1]
+	x2 = boxes[:, 2]
+	y2 = boxes[:, 3]
+
+	if boxes.dtype.kind == "i":
+		boxes = boxes.astype("float")
+
+	# initialize the list of picked indexes
+	pick = []
+
+	# calculate the areas
+	area = (x2 - x1) * (y2 - y1)
+
+	# sort the bounding boxes
+	idxs = np.argsort(probs)
+	idx_overlap =[]
+	# keep looping while some indexes still remain in the indexes
+	# list
+	while len(idxs) > 0:
+		# grab the last index in the indexes list and add the
+		# index value to the list of picked indexes
+		last = len(idxs) - 1
+		i = idxs[last]
+		pick.append(i)
+
+		# find the intersection
+
+		xx1_int = np.maximum(x1[i], x1[idxs[:last]])
+		yy1_int = np.maximum(y1[i], y1[idxs[:last]])
+		xx2_int = np.minimum(x2[i], x2[idxs[:last]])
+		yy2_int = np.minimum(y2[i], y2[idxs[:last]])
+
+		ww_int = np.maximum(0, xx2_int - xx1_int)
+		hh_int = np.maximum(0, yy2_int - yy1_int)
+
+		area_int = ww_int * hh_int
+
+		# find the union
+		area_union = area[i] + area[idxs[:last]] - area_int
+
+		# compute the ratio of overlap
+		overlap = area_int/(area_union + 1e-6)
+
+		# delete all indexes from the index list that have
+		idx_overlap.append(np.concatenate(([last],np.where(overlap > overlap_thresh)[0])))
+		idxs = np.delete(idxs, np.concatenate(([last],np.where(overlap > overlap_thresh)[0])))
+
+
+	# return only the bounding boxes that were picked using the integer data type
+
+	return idx_overlap
